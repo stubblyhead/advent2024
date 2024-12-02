ea0e8b8191b58fac671ec3b8f50c3e84781fc58b
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-01 23:47:16 -0800

    part 2 passing test, but too low on input

--- day2/isitsafe.py
+++ day2/isitsafe.py
@@ -1,4 +1,4 @@
-reports = open('input').readlines()
+reports = open('testcase').readlines()
 
 safecount = 0
 for l in reports:
@@ -26,5 +26,38 @@ for l in reports:
                 break 
     if safe:  #didn't find a reason to mark the report unsafe
         safecount += 1 
+
+print(safecount)
+
+#part 2
+safecount = 0
+for r in reports:
+    levels = list(map(int,r.split()))
+    safe = True  #still assume true and try to prove false
+    damped = False #keep track of if damper has triggered for this report
+    increasing = None #don't really like how I'm doing this, feels yucky
+    if levels[0] > levels[-1]:  #generally decreasing
+            increasing = False
+    elif levels[0] < levels[-1]:  #generally increasing
+            increasing = True
+    else:  #generally static (not safe), so go to next report
+        next
+    
+    while len(levels) > 1:
+        cur = levels.pop() #get one off the end
+        if increasing:
+            if cur-levels[-1] in [1,2,3]: #last two differ by at least one and no more than three
+                continue #can check new last pair
+        else:
+            if levels[-1]-cur in [1,2,3]:
+                continue
+        if damped:
+            safe = False #second unsafe level change for this report, so entire report is unsafe
+            break #don't need to keep checking this one
+        else:
+            levels[-1] = cur #replace last item with one we popped previously and try again
+            damped = True
+    if safe:
+        safecount += 1
 
 print(safecount)
