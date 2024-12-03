c0cdea317b0a0a45b1528ada4d89fc477a57d938
Mike Stevenson <miksteve@amazon.com>
2024-12-03 18:18:22 +0000

    making the multiplying a function, shoud have done this to begin with

--- day3/multfind.py
+++ day3/multfind.py
@@ -1,13 +1,18 @@
 import regex as re
 
 mem = open('input').readlines()
-
-pattern = "mul\(\d+,\d+\)"
-total = 0
-for m in mem:
+def do_mult(memstr):
+    pattern = "mul\(\d+,\d+\)"
+    total = 0
     matches = re.findall(pattern, m)
     for i in matches:
         args = i[4:-1].split(',')
         total += int(args[0]) * int(args[1])
+    return total
+
+ans = 0
+for m in mem:
+    ans += do_mult(m)
+print(ans)
 
 print(total)
