a431c4de68386645638ce5e2c43230e09b18bfe2
Mike Stevenson <miksteve@amazon.com>
2024-12-05 02:26:47 +0000

    i really hate how I'm doing this, but I can't think of a way that wouldn't be just as ugly in other ways

--- day4/wordsearch.py
+++ day4/wordsearch.py
@@ -1,7 +1,107 @@
-lines = open('testcase').readlines()
+#calling it now, part 2 will be with rows/cols wrapping around
 
-grid = []
-for l in lines:
+class Grid:
+    def __init__(self, lines):
+        self.grid = []
+        for l in lines:
+            self.grid.append(list(l.strip()))
+        self.height = len(self.grid)
+        self.width = len(self.grid[0])
+
+    def valid_dirs(self, row, col):
+        """
+        in: row and column of 'X' character
+        out: list of valid directions to look for word going clockwise from upper left
+        """
+        u = row-3 >= 0
+        d = self.height-row >= 4
+        l = col-3 >= 0
+        r = self.width-col >= 4
+        return [u and l, u, u and r, r, d and r, d, d and l, l]
     
+    def get_xmas_count(self,row,col):
+        valid = self.valid_dirs(row,col)
+        count = 0
+        if valid[0]:  # i hate this, but I'm not sure how else to do it 
+            word = self.grid[row][col]
+            t_row,t_col = row,col
+            for i in range(3):
+                t_row -= 1
+                t_col -= 1
+                word += self.grid[t_row][t_col]
+        if word == 'XMAS':
+            count += 1
+       
+        if valid[1]:
+            word = self.grid[row][col]
+            t_row = row
+            for i in range(3):
+                t_row -= 1
+                word += self.grid[t_row][col]
+        if word == 'XMAS':
+            count += 1
+        
+        if valid[2]:
+            word = self.grid[row][col]
+            t_row,t_col = row,col
+            for i in range(3):
+                t_row -= 1
+                t_col += 1
+                word += self.grid[t_row][t_col]
+        if word == 'XMAS':
+            count += 1
 
-print(grid)
+        if valid[3]:
+            word = self.grid[row][col]
+            t_col = col
+            for i in range(3):
+                t_col -= 1
+                word += self.grid[row][t_col]
+        if word == 'XMAS':
+            count += 1
+
+        if valid[4]:
+            word = self.grid[row][col]
+            t_col = col
+            for i in range(3):
+                t_col += 1
+                word += self.grid[row][t_col]
+        if word == 'XMAS':
+            count += 1
+
+        if valid[5]:  
+            word = self.grid[row][col]
+            t_row,t_col = row,col
+            for i in range(3):
+                t_row += 1
+                t_col -= 1
+                word += self.grid[t_row][t_col]
+        if word == 'XMAS':
+            count += 1
+       
+        if valid[6]:
+            word = self.grid[row][col]
+            t_row = row
+            for i in range(3):
+                t_row += 1
+                word += self.grid[t_row][col]
+        if word == 'XMAS':
+            count += 1
+        
+        if valid[7]:
+            word = self.grid[row][col]
+            t_row,t_col = row,col
+            for i in range(3):
+                t_row += 1
+                t_col += 1
+                word += self.grid[t_row][t_col]
+        if word == 'XMAS':
+            count += 1
+        return count
+
+
+lines = open('testcase').readlines()
+my_grid = Grid(lines)
+for row in range(len(my_grid.grid)):
+    for col in range(len(my_grid.grid[0])):
+        print(f"{row} {col} {my_grid.valid_dirs(row,col)}")
