99dda0de5c50f93c311c4c14f6b5d44c2e2e0180
Mike Stevenson <miksteve@amazon.com>
2024-12-06 01:54:46 +0000

    something ain't right, not sure what yet

--- day5/milton.py
+++ day5/milton.py
@@ -1,7 +1,8 @@
-lines = open('input').readlines()
+lines = open('testcase').readlines()
 
 rules = []
 manuals = []
+incorrect_manuals = []
 
 for l in lines:
     if l.count('|'):  # I don't think we really need to cast as int at this point, but maybe in part 2
@@ -21,6 +22,7 @@ for m in manuals:
                 next  # go to next rule, this one is in compliance
             else:
                 valid = False  # otherwise manual is not valid so we can skip the rest of the rules
+                incorrect_manuals.append(m)
                 break
         # don't need to do anything if manual doesn't contain all in r
 
@@ -31,4 +33,34 @@ print(count)
 
 # rules are cylical, not really a single "order," would depend on the pages in the manual
 # I think I need to build a page order for each manual, filter out rules that include pages not in the manual
-applicable_rules = []
+
+count = 0
+for m in incorrect_manuals:
+    applicable_rules = []
+    for r in rules:
+        if r[0] in m and r[1] in m:
+            applicable_rules.append(r)
+    rule_dict = {}
+    left_rules = []
+    right_rules = []
+    for r in applicable_rules:
+        if r[0] in rule_dict.keys():
+            rule_dict[r[0]].append(r[1])
+        else:
+            rule_dict[r[0]] = [r[1]]
+            right_rules.append(r[1])
+    left_rules = list(rule_dict.keys())
+    order = []
+    while left_rules:
+        for l in left_rules:
+            if l not in right_rules:
+                order.append(l)
+                for r in rule_dict[l]:
+                    right_rules.remove(r)
+                left_rules.remove(l)
+                break
+    count += m[int(len(m)/2)]
+    
+
+
+    
