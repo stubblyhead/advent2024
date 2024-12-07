d80cebf6d01c2c4c12aacf5298c35e7169dea9b7
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-07 15:09:27 -0800

    testcase works for part 2. i'll put the over/under for the real input at 10min

--- day7/gotohellauntsally.py
+++ day7/gotohellauntsally.py
@@ -1,42 +1,55 @@
 from operator import add, mul
-ops = {'0':add,'1':mul}  # this makes me feel kind of dirty but it beats 10 billion calls to int()
-lines = open('input').readlines()
+def concat(a,b):
+    return int(str(a)+str(b))
+
+ops = {'0':add,'1':mul,'2':concat}  # this makes me feel kind of dirty but it beats 10 billion calls to int()
+lines = open('testcase').readlines()
 
 def bin(s):
     return str(s) if s<=1 else bin(s>>1) + str(s&1)
 
+def num_to_base(n, b):
+    if n == 0:
+        return [0]
+    digits = []
+    while n:
+        digits.append(int(n % b))
+        n //= b
+    return digits[::-1]
+
+
+
 eqns = {}
 for l in lines:
     a,b = l.split(':')
-    eqns[int(a)] = list(map(int,b.split()))
-
-"""
-TODO need to potentially check all combinations of + and *.
-strategy--2^n possible combinations where n = len(rhs)-1,
-so iterate through that many times, shifting off bits as we 
-go.  probably won't scale well for large n, but should be 
-some ways to mitigate that.  maybe leverage operator module?
-might be too inefficient to make so many function calls.
-"""
+    eqns[int(a)] = tuple(map(int,b.split()))
 
 total = 0
 
-for target in eqns.keys():
+def check_if_valid(target, operands, base):
     perm = len(eqns[target]) - 1
     found = False
-    for i in range(pow(2,perm)):
-        operands = eqns[target] # will let me manipulate list without affecting future iterations
+    for i in range(pow(base,perm)):
+        this_operands = list(operands) # will let me manipulate list without affecting future iterations
         this_total = 0
-        binstr = '{:>0{length}}'.format(bin(i),length=perm)
-        for i in binstr:
-            this_total = ops[i](operands[0],operands[1])
-            operands = [this_total]+operands[2:]
-        if operands[0] == target:
-            total += target
+        i = num_to_base(i,base)
+        binstr = '{:>0{length}}'.format(''.join(map(str,i)),length=perm)
+        for j in binstr:
+            this_total = ops[j](this_operands[0],this_operands[1])
+            this_operands = [this_total]+this_operands[2:]
+        if this_operands[0] == target:
             found = True
-        if found:
             break
+    return found
 
+for k,v in eqns.items():
+    if check_if_valid(k,v,2):
+        total += k
 print(total)
         
+total = 0
+for k,v in eqns.items():
+    if check_if_valid(k,v,3):
+        total += k
+print(total)
 
