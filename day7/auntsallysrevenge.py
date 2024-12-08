9b0a130926af0a5a2753e776e18b9719b519c691
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-07 23:39:18 -0800

    goes a lot faster going backwards

--- day7/auntsallysrevenge.py
+++ day7/auntsallysrevenge.py
@@ -15,12 +15,17 @@ def num_to_base(n, b):
     return digits[::-1]
 
 def check_valid(target, operands, base):
-    perms = pow(base,len(operands)-1)
-    for perm in perms:
+    places = len(operands)-1
+    perms = pow(base,places)
+    for perm in range(perms):
         this_target = target
         this_operands = list(operands) # will let me manipulate list without affecting future iterations
         
         perm = num_to_base(perm,base)
+        if base == 3 and 2 not in perm:  # can skip this permutation if there's no concatenation because we already checked it
+            continue  
+        for _ in range(places-len(perm)):
+            perm = [0] + perm
         for i in perm:
             bust = False
             if i == 0:  # addition
@@ -37,21 +42,34 @@ def check_valid(target, operands, base):
                     this_target //= last_num
             elif i == 2:  # concatenation
                 last_num = str(this_operands.pop()) # convert last number to a string
-                if last_num != this_target[len(last_num):]:
+                if last_num != str(this_target)[len(str(this_target))-len(last_num):]:
                     bust = True
                     break
                 else:
-                    this_target = this_target[:len(last_num)]
+                    this_target = int(str(this_target)[:len(last_num)+1])
         if bust:
-            return False
+            next
         elif this_target == operands[0]:
             return True
-        else:
-            return False
+        # else:
+        #     return False
         
 total = 0
+valid = []
 for k,v in eqns.items():
     if check_valid(k,v,2):
+        valid.append(k)  # keep track of valid equations so we can remove them later
         total += k
 print(total)
+for i in valid:
+    eqns.pop(i)  # remove the ones we already know are valid
+
+total2 = 0
+for k,v in eqns.items():
+    if check_valid(k,v,3):
+        # print(f'{k}: {v} is valid')
+        total2+= k
+    # else:
+        # print(f'{k}: {v} is not valid')
+print(total+total2)
                 
