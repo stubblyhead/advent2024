74fe159bef7c6e0e629e34910486e2f497437c8c
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-12 09:04:25 -0800

    this may not be useful

--- day9/refrag.py
+++ day9/refrag.py
@@ -11,3 +11,12 @@ while len(diskmap) > 1:
     freespace.append(int(diskmap.pop(0)))
 files.append(int(diskmap.pop()))
 
+print(files, freespace)
+gaplength = sum(freespace)  # upper bound for how many blocks need to move
+backfill = []
+rev_files = files[::-1]
+backfill_total = 0
+for i in range(len(files)-1,-1,-1):
+    if backfill_total > gaplength:
+        break
+    backfill_total.append([i,])
