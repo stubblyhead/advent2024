4e9395b3fe9b2086dd528da2aeacca6abe9626c6
Mike Stevenson <miksteve@amazon.com>
2024-12-13 00:55:50 +0000

    test input passes but real input doesn't, not sure what's going on

--- day9/refrag.py
+++ day9/refrag.py
@@ -1,4 +1,4 @@
-diskmap = list(open('testcase').readline().strip())
+diskmap = list(open('input').readline().strip())
 
 # making it a single string seems like a trap, but i might end up
 # doing it anyway
@@ -10,13 +10,36 @@ while len(diskmap) > 1:
     files.append(int(diskmap.pop(0)))
     freespace.append(int(diskmap.pop(0)))
 files.append(int(diskmap.pop()))
-
-print(files, freespace)
+filelength = sum(files)
 gaplength = sum(freespace)  # upper bound for how many blocks need to move
 backfill = []
-rev_files = files[::-1]
 backfill_total = 0
 for i in range(len(files)-1,-1,-1):
     if backfill_total > gaplength:
         break
-    backfill_total.append([i,])
+    backfill.append([i,files[i]])
+    backfill_total += files[i]
+
+refragged = []
+file_count = len(files)
+for i in range(file_count):
+    this_files = files.pop(0)
+    refragged += [ i for _ in range(this_files) ]
+    if freespace:
+        this_freespace = freespace.pop(0)
+        to_move = []
+        while len(to_move) < this_freespace:
+            backfill_val, backfill_count = backfill.pop(0)
+            to_move += [ backfill_val for _ in range(min(backfill_count, this_freespace)) ]
+        to_move = to_move[:this_freespace]
+        extra = backfill_count - to_move.count(backfill_val)
+        if extra:
+            backfill = [[backfill_val,extra]] + backfill
+        refragged += to_move
+            
+refragged = refragged[:filelength]
+checksum = 0
+for i in range(len(refragged)):
+    checksum += i * refragged[i]
+
+print(checksum)
