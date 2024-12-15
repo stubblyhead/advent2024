429f7ec4dc5b0653b25250b2997043ba3ee55b90
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-14 19:38:24 -0800

    whoops solved part 2 first lol

--- day10/hiking.py
+++ day10/hiking.py
@@ -1,4 +1,4 @@
-lines = open('testcase').readlines()
+lines = open('input').readlines()
 grid = []
 for l in lines:  # going to try padding grid in an attempt to avoid OOB errors
     grid.append([-1] + list(map(int,l.strip())) + [-1])
@@ -7,11 +7,13 @@ grid = [[ -1 for _ in range(len(grid[0])) ]] + grid + [[ -1 for _ in range(len(g
 def bfs(g, root):
     horizon = [root]
     nines = set()
+    nines_count = 0
     while horizon:
         cur_row,cur_col = horizon.pop(0)
         cur_val = g[cur_row][cur_col]
         if cur_val == 9:
             nines.add((cur_row,cur_col))
+            nines_count += 1
             continue
         if g[cur_row+1][cur_col] == cur_val + 1:
             horizon.append([cur_row+1,cur_col])
@@ -21,15 +23,19 @@ def bfs(g, root):
             horizon.append([cur_row,cur_col+1])
         if g[cur_row][cur_col-1] == cur_val + 1:
             horizon.append([cur_row,cur_col-1])
-    return len(nines)
+    return [len(nines),nines_count]
 
-count = 0
+count1 = 0
+count2 = 0
 for row in range(len(grid)):
     for col in range(len(grid[0])):
         if grid[col][row] == 0:
-            count += bfs(grid, [col,row])
+            ans = bfs(grid, [col,row])
+            count1 += ans[0]
+            count2 += ans[1]
 
-print(count)
+print(count1)
+print(count2)
     
         
             
