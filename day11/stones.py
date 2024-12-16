d32f0720e019a7ff47c8576f44aacbd8cab6d5cd
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-16 07:47:28 -0800

    it's log

--- day11/stones.py
+++ day11/stones.py
@@ -1,5 +1,24 @@
 test1 = '0 1 10 99 999'
 test2 = '125 17'
 input = '8069 87014 98 809367 525 0 9494914 5'
+from math import log, floor
 
 stones = list(map(int,test1.split()))
+
+# this will probably require an absurd number of calcs; memoization might be a good idea
+
+for _ in range(1):
+    new_stones = []
+    for i in stones:
+        if i == 0:
+            new_stones += [1]
+        elif floor(log(i,10)) % 2 == 1:
+            p = floor(log(i,10))
+            l = i // pow(10,p-1)
+            r = i-l*pow(10,p-1)
+            new_stones += [l,r]
+        else:
+            new_stones += [i * 2024]
+    stones = new_stones
+
+print(new_stones)
