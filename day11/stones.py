30adf3f66aa5dfc250b2be0fa4ab8420f45a0995
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-16 20:24:24 -0800

    part 1 complete.  predicting now that part 2 w/ this solution will take hours

--- day11/stones.py
+++ day11/stones.py
@@ -3,22 +3,32 @@ test2 = '125 17'
 input = '8069 87014 98 809367 525 0 9494914 5'
 from math import log, floor
 
-stones = list(map(int,test1.split()))
+stones = list(map(int,input.split()))
 
 # this will probably require an absurd number of calcs; memoization might be a good idea
 
-for _ in range(1):
+def get_left_half(num):
+    if floor(log(num,10)) % 2 == 1:
+        p = floor(log(num,10))
+        l = num // pow(10,p//2+1)
+        return l
+
+
+    else:
+        return -1
+
+for _ in range(25):
     new_stones = []
     for i in stones:
         if i == 0:
             new_stones += [1]
         elif floor(log(i,10)) % 2 == 1:
             p = floor(log(i,10))
-            l = i // pow(10,p-1)
-            r = i-l*pow(10,p-1)
+            l = i // pow(10,p//2+1)
+            r = i-l*pow(10,p//2+1)
             new_stones += [l,r]
         else:
             new_stones += [i * 2024]
     stones = new_stones
 
-print(new_stones)
+print(len(stones))
