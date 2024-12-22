c1a75947cb22024e44f24597ea7ae349acdab42f
stubblyhead <27823133+stubblyhead@users.noreply.github.com>
2024-12-22 10:38:02 -0800

    very large numbers are messing with integer detection logic

--- day13/clawgame.py
+++ day13/clawgame.py
@@ -17,6 +17,25 @@ for g in games:
     if np.allclose(np.round(solution).astype(int),solution):
         count += 1
         coins += (solution[0]*3 + solution[1])
+        
     
+print(f'{count} of {len(games)} have a solution')
+print(coins)
 
+coins = 0
+count = 0
+for g in games:
+    numbers = [ int(i) for i in re.findall(pattern, g) ]
+    a_x, a_y, b_x, b_y, prize_x, prize_y = numbers
+    
+    # it's all simultaneous equations-- a_x*x + b_x*x = prize_x*x, a_y*y + b_y*y = prize_y*y; solve for x and y
+    buttons = np.array([[a_x, b_x],[a_y,b_y]])
+    prize = np.array([prize_x+10000000000000, prize_y+10000000000000])
+    solution = np.linalg.solve(buttons, prize)
+    rounded = np.round(solution).astype(int)
+    if np.allclose(rounded,solution):
+        count += 1
+        coins += (rounded[0]*3 + rounded[1])
+        print(solution[0], solution[1])
+print(f'{count} of {len(games)} have a solution')
 print(coins)
