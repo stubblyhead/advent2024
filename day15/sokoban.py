9601a587cdef60425713cc6e9acd13d87aed2ea4
Mike Stevenson <miksteve@amazon.com>
2024-12-23 21:32:48 +0000

    move functions; kind of torn between splitting them or making a monofunction with conditionals=

--- day15/sokoban.py
+++ day15/sokoban.py
@@ -3,5 +3,60 @@ warehouse = [ list(w.strip()) for w in warehouse.split() ]
 
 for row in range(len(warehouse)):
     if warehouse[row].count('@'):
-        robot = (row, warehouse[row].index('@'))
+        robot = [row, warehouse[row].index('@')]
+        warehouse[robot[0]][robot[1]] = '.'
+
+moves = list(moves)
+
+def move_up(robot,warehouse):
+    r,c = robot
+    adj = warehouse[r-1][c]
+    if adj == '.':
+        robot[0] -= 1
+
+    elif adj == '#':
+        return
+    
+    else:
+        pass
+        #TODO obstacle fuckery
+
+def move_down(robot,warehouse):
+    r,c = robot
+    adj = warehouse[r+1][c]
+    if adj == '.':
+        robot[0] += 1
+
+    elif adj == '#':
+        return
+    
+    else:
+        pass
+        #TODO obstacle fuckery
+
+def move_left(robot,warehouse):
+    r,c = robot
+    adj = warehouse[r][c-1]
+    if adj == '.':
+        robot[1] -= 1
+
+    elif adj == '#':
+        return
+    
+    else:
+        pass
+        #TODO obstacle fuckery
+
+def move_up(robot,warehouse):
+    r,c = robot
+    adj = warehouse[r][c+1]
+    if adj == '.':
+        robot[1] += 1
+
+    elif adj == '#':
+        return
+    
+    else:
+        pass
+        #TODO obstacle fuckery
 
