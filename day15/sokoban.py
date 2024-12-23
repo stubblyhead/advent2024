db2839e127a02f9b394cbe53dc7aba5c44e5eb80
Mike Stevenson <miksteve@amazon.com>
2024-12-23 23:24:11 +0000

    added collision logic

--- day15/sokoban.py
+++ day15/sokoban.py
@@ -4,7 +4,6 @@ warehouse = [ list(w.strip()) for w in warehouse.split() ]
 for row in range(len(warehouse)):
     if warehouse[row].count('@'):
         robot = [row, warehouse[row].index('@')]
-        warehouse[robot[0]][robot[1]] = '.'
 
 moves = list(moves)
 
@@ -13,50 +12,97 @@ def move_up(robot,warehouse):
     adj = warehouse[r-1][c]
     if adj == '.':
         robot[0] -= 1
+        warehouse[r][c] = '.'
+        warehouse[r-1][c] = '@'
 
     elif adj == '#':
         return
     
     else:
-        pass
-        #TODO obstacle fuckery
+        adj_to_edge = [ i[c] for i in warehouse[r-1::-1] ]
+        if adj_to_edge.count('.') == 0:  # nowhere for boxes to go so just quit
+            return
+        elif adj_to_edge.index('.') < adj_to_edge.index('#'): # there's an empty space between the obstacle and the nearest wall
+            warehouse[r-1-adj_to_edge.index('.')][c] = 'O' # adjacent box moves into that empty space
+            robot[0] -= 1 # robot moves into new empty space
+            warehouse[r][c] = '.'
+            warehouse[r-1][c] = '@'
 
 def move_down(robot,warehouse):
     r,c = robot
     adj = warehouse[r+1][c]
     if adj == '.':
         robot[0] += 1
+        warehouse[r][c] = '.'
+        warehouse[r+1][c] = '@'
 
     elif adj == '#':
         return
     
     else:
-        pass
-        #TODO obstacle fuckery
+        adj_to_edge = [ i[c] for i in warehouse[r+1:] ]
+        if adj_to_edge.count('.') == 0:  # nowhere for boxes to go so just quit
+            return
+        elif adj_to_edge.index('.') < adj_to_edge.index('#'): # there's an empty space between the obstacle and the nearest wall
+            warehouse[r+1+adj_to_edge.index('.')][c] = 'O' # adjacent box moves into that empty space
+            robot[0] += 1 # robot moves into new empty space
+            warehouse[r][c] = '.'
+            warehouse[r+1][c] = '@'
 
 def move_left(robot,warehouse):
     r,c = robot
     adj = warehouse[r][c-1]
     if adj == '.':
         robot[1] -= 1
+        warehouse[r][c] = '.'
+        warehouse[r][c-1] = '@'
 
     elif adj == '#':
         return
     
     else:
-        pass
-        #TODO obstacle fuckery
+        adj_to_edge = warehouse[r][c-1::-1]
+        if adj_to_edge.count('.') == 0:  # nowhere for boxes to go so just quit
+            return
+        elif adj_to_edge.index('.') < adj_to_edge.index('#'): # there's an empty space between the obstacle and the nearest wall
+            warehouse[r][c-1-adj_to_edge.index('.')] = 'O' # adjacent box moves into that empty space
+            robot[1] -= 1 # robot moves into new empty space
+            warehouse[r][c] = '.'
+            warehouse[r][c-1] = '@'
 
-def move_up(robot,warehouse):
+def move_right(robot,warehouse):
     r,c = robot
     adj = warehouse[r][c+1]
     if adj == '.':
         robot[1] += 1
+        warehouse[r][c] = '.'
+        warehouse[r][c+1] = '@'
 
     elif adj == '#':
         return
     
     else:
-        pass
-        #TODO obstacle fuckery
+        adj_to_edge = warehouse[r][c+1:]
+        if adj_to_edge.count('.') == 0:  # nowhere for boxes to go so just quit
+            return
+        elif adj_to_edge.index('.') < adj_to_edge.index('#'): # there's an empty space between the obstacle and the nearest wall
+            warehouse[r][c+1+adj_to_edge.index('.')] = 'O' # adjacent box moves into that empty space
+            robot[1] += 1 # robot moves into new empty space
+            warehouse[r][c] = '.'
+            warehouse[r][c+1] = '@'
 
+def print_warehouse(w):
+    for r in range(len(w)):
+        print(''.join(w[r]))
+
+for m in moves:
+    if m == '>':
+        move_right(robot, warehouse)
+    elif m == '<':
+        move_left(robot, warehouse)
+    elif m == '^':
+        move_up(robot, warehouse)
+    elif m == 'v':
+        move_down(robot, warehouse)
+
+print_warehouse(warehouse)
