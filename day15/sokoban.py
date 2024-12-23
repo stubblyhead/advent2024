63cc809fde48bafe3183c6ef491e33628c809301
Mike Stevenson <miksteve@amazon.com>
2024-12-23 23:28:05 +0000

    looks like move functions are working correctly

--- day15/sokoban.py
+++ day15/sokoban.py
@@ -1,4 +1,4 @@
-warehouse, moves = list(open('testcase1').read().split('\n\n'))
+warehouse, moves = list(open('testcase2').read().split('\n\n'))
 warehouse = [ list(w.strip()) for w in warehouse.split() ]
 
 for row in range(len(warehouse)):
